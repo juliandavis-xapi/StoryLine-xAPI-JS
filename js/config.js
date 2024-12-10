8fc2658b0681f4ed1fdd8f52662744f5103a6784
xAPI.com.au <julian@xapi.com.au>
2024-12-10 10:11:30 +1000

    Update config.js
    Added Dynamic HomePage from Window Location used in the Agent

--- js/config.js
+++ js/config.js
@@ -6,13 +6,20 @@ Home page is used when creating the actor Agent
 
 */
 
+var homePage="";
+try{
+   homePage = window.location.protocol +'//'+ window.location.hostname;
+}catch(err){
+   homePage = "https://connectandlearn.isq.qld.edu.au/";
+}
+
 lrsData = {
 	"authkey": "username:password",
 	"endpoint": "https://your.lrs/xapi/",
 	"theLaunchURL": "index_lms.html",
 	"activityID":"http://this-is-the-activity",
 	"showConsole": true, //This will show the xAPI object in the Browser Console
-	"homePage": "https://yourhomepage.comu"
+	"homePage": homePage
 }
 
 
